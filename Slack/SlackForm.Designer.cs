f42dd7b7458cb4f1a3252c3953f269f776514140
Toru Nayuki <tnayuki@icloud.com>
2014-09-27 22:53:18 +0900

    Initial commit

---
+++ Slack/SlackForm.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Slack
+{
+    partial class SlackForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SlackForm));
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.IsWebBrowserContextMenuEnabled = false;
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(1024, 761);
+            this.webBrowser.TabIndex = 0;
+            this.webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
+            // 
+            // SlackForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1024, 761);
+            this.Controls.Add(this.webBrowser);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SlackForm";
+            this.Text = "Slack";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SlackForm_FormClosing);
+            this.Load += new System.EventHandler(this.SlackForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+    }
+}
+
---
+++ Slack/SlackForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Slack
+{
+    public partial class SlackForm : Form
+    {
+        public SlackForm()
+        {
+            InitializeComponent();
+
+            webBrowser.Url = new Uri("https://slack.com/signin");
+        }
+
+        private void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            //webBrowser.Navigate(new Uri("javascript:window.Notification = function(title, options) { window.alert(title); }; window.Notification.permission = 'granted'; undefined;"));
+        }
+
+        private void SlackForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (WindowState == FormWindowState.Normal)
+            {
+                Properties.Settings.Default.Bounds = Bounds;
+            }
+            else
+            {
+                Properties.Settings.Default.Bounds = RestoreBounds;
+            }
+
+            Properties.Settings.Default.WindowState = WindowState;
+
+            Properties.Settings.Default.Save();
+        }
+
+        private void SlackForm_Load(object sender, EventArgs e)
+        {
+            Bounds = Properties.Settings.Default.Bounds;
+            WindowState = Properties.Settings.Default.WindowState;
+        }
+    }
+}
